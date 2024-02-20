014c46197c07c7d713a9558e3c182983f0ef9c46
HMD <hamid.elec@outlook.com>
2024-02-20 08:21:49 +0330

    add anoder define Blue LEd

--- BlinksomeLEDs/BlinksomeLEDs.ino
+++ BlinksomeLEDs/BlinksomeLEDs.ino
@@ -1,6 +1,7 @@
 // this code is an example to work with git hub abd git on the computer 
 
 #define RedLED 6 
+#define BlueLED 8
 
 void setup() {
   
@@ -11,9 +12,9 @@ void setup() {
 
 void loop() {
   digitalWrite(RedLED, 1);  
-  digitalWrite(8, 0);  
+  digitalWrite(BlueLED, 0);  
   delay(250);                      
   digitalWrite(RedLED, 0); 
-  digitalWrite(8, 1);    
+  digitalWrite(BlueLED, 1);    
   delay(500);                     
 }
